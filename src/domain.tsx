27245bc91bd9afd78951871ed157a8eee1b24903
Benni-beep <krikelkr4kel@gmail.com>
2023-11-17 09:13:45 +0100

    feat: add validation for technologies-list

--- src/domain.tsx
+++ src/domain.tsx
@@ -1,11 +1,30 @@
 import { Technology } from "./types";
 
 export const getTechIcon = (technology: Technology) => {
-  return <img className="tech-icon" src={`./icons/icons8-${technology}.svg`} alt="" />;
+  return (
+    <img
+      className="tech-icon"
+      src={`./icons/icons8-${technology}.svg`}
+      alt=""
+    />
+  );
 };
 
 export const getTechIconList = (technologies: Technology[]) => {
-    return <div className="tech-icon-list">
-        {technologies.map(technology => getTechIcon(technology))}
+  return (
+    <div className="tech-icon-list">
+      {technologies.map((technology) => getTechIcon(technology))}
     </div>
+  );
+};
+
+export const isTechnology = (value: unknown): value is Technology => {
+    const validTechnologies: Technology[] = ["css3", "github", "html5", "javascript", "nodejs", "photoshop", "react", "typescript"];
+    return typeof value === 'string' && validTechnologies.includes(value as Technology);
+};
+
+export const validateTechnologies = (
+  technologies: string[]
+): technologies is Technology[] => {
+  return technologies.every(isTechnology);
 };
