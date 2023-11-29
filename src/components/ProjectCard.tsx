3c9e348e0c9ae5fa5125451534b5e01610f85657
Benni-beep <krikelkr4kel@gmail.com>
2023-11-29 12:34:27 +0100

    fix ProjectCard layout

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,10 @@
-import { getTechIconList, validateTechnologies } from "../domain";
+import {
+  getTechIconList,
+  validateTechnologies,
+} from "../domain";
 import { ProjectCardProps } from "../types";
+
+import "./ProjectCard.css"
 
 export const ProjectCard = (props: ProjectCardProps) => {
   const { name, image, technologies, repoUrl, liveUrl } = props;
@@ -7,20 +12,27 @@ export const ProjectCard = (props: ProjectCardProps) => {
   const validTechnologies = validateTechnologies(technologies);
   const techIconList = getTechIconList(validTechnologies);
 
+  const projectCardStyle = { 
+    backgroundImage: `url(${image})`,
+  };
+
   return (
-    <div className="project-card">
-      <label>
-        {name}
-        <a href={liveUrl}>
-          <img src={`./images/${image}`} alt="" />
-        </a>
-      </label>
-      <div className="icons">
-        <a href={repoUrl}>
-          <img src="./icons/technologies/github.svg" alt="" />
-        </a>
-        <div className="technology-icons">{techIconList}</div>
+    <a href={liveUrl} target="_blank" rel="noopener noreferrer" className="project-card-link">
+      <div className="project-card" style={projectCardStyle}>
+        <div className="card-content">
+          <label className="project-name">
+            {name}
+          </label>
+          <div className="icons">
+            <a href={repoUrl} className="repo-link">
+              <img src="./icons/technologies/github.svg" alt="GitHub Repo" />
+            </a>
+            <div className="technology-icons">
+              {techIconList}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
